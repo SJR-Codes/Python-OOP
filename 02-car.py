a0f6bcf7b4a65af3489f339b831caa22a600d4fe
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-21 11:02:21 +0300

    2. exercise created

---
+++ 02-car.py
@@ -0,0 +1,22 @@
+"""
+* Taitotalo Python 21.10.2022
+* 02-car.py
+* OOP exercizing continued
+* Created by Samu Reinikainen
+"""
+
+class Car():
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+
+    def __str__(self):
+        return f"{self.brand} {self.model} {str(self.year)}"
+
+skoda = Car("Skoda","Fabia",2000)
+volkkari = Car("Volkswagen","Golf Variant",2022)
+
+print(skoda)
+print(volkkari)
