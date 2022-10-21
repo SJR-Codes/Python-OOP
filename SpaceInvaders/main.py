c8a57c00d88e6703c220e646dff86a421c6092c5
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-10-21 14:25:18 +0300

    we have window with caption

--- SpaceInvaders/main.py
+++ SpaceInvaders/main.py
@@ -5,5 +5,30 @@
 * Created by Samu Reinikainen
 """
 
-import pygame
+import pygame as pg
+import sys
 
+class SpaceInvaders:
+    def __init__(self):
+        #initialize pygame
+        pg.init()
+        self.screen = pg.display.set_mode((1200,800))        
+        pg.display.set_caption("Space Invaders")
+
+    def run(self):
+        #main loop
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    sys.exit()
+            pg.display.flip()
+    
+
+def main():
+    game = SpaceInvaders()
+    game.run()
+
+
+# run blocks of code only if our program is the main program executed
+if __name__ == "__main__":
+    main()
